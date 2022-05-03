3d2fdba20abada2f9018120421d15ab22e101ded
akschoenbauer <assc1433@colorado.edu>
2022-05-03 17:53:42 -0600

    one last attempt at fixing nav bar

--- javascript/java.js
+++ javascript/java.js
@@ -1,20 +1,4 @@
 // DROP DOWN NAVBAR MENU //
-/*
-function dropdownFunction() {
-  document.getElementById('workDropDown').classList.toggle('show');
-}
-
-window.onclick = function(e) {
-  if (!e.target.matches('.workdropbtn')) {
-    var workDropDown = document.getElementById('workDropDown');
-      if (workDropDown.classList.contains('show')) {
-        workDropDown.classList.remove('show');
-      }
-  }
-}
-*/
-
-// w3 schools demo
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
